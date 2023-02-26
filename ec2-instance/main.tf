f1a9803b5184b8f0302db0dbfa26ffad1f96c352
Madhumitha611999 <madhu312128@gmail.com>
2023-02-26 22:50:11 +0530

    test

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -14,14 +14,14 @@ resource "aws_security_group" "example" {
     from_port = 0
     to_port   = 0
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
   egress {
     from_port = 0
     to_port   = 0
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
 }

7be0b55d5c012e4e9f2ea97a0d8e339faf1cc004
Madhumitha611999 <madhu312128@gmail.com>
2023-02-26 22:48:26 +0530

    test

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -7,12 +7,32 @@ resource "aws_subnet" "example" {
   cidr_block = "10.0.1.0/24"
 }
 
+resource "aws_security_group" "example" {
+  name_prefix = "example"
+  #vpc_id      = vpc-0b097c09de2c10c10
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
 
 resource "aws_instance" "exampleusingVPC" {
   ami                    = "ami-024fc608af8f886bc"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.example.id
-  vpc_security_group_ids = ["sg-077fcd7a53e5cd890"]
+  vpc_security_group_ids = [aws_security_group.example.id]
   key_name               = "Keypair"
   tags = {
     Name        = "example-instance"
